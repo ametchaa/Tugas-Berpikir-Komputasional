d61147147061ef2b5d2149e8f427491f97412e1d
ametchaa <alfianjani2927@gmail.com>
2026-09-20 06:58:50 +0700

    Add files via upload

---
+++ Latihan Hal 29.py
@@ -0,0 +1,32 @@
+# Program Latihan
+# Latihan type data dan assignment
+
+# KAMUS
+# IA : int
+# FA, FB : float
+# SA, SB : string
+# BA : bool
+# CA, CB : char
+
+# Algoritma
+IA = 10           # (1)
+print(IA)
+FA = 3.45         # (2)
+print(FA)
+FB = 4.567        # (3)
+print(FB)
+FB = IA           # (4)
+print(FB)
+SA = "ITBJAYA"    # (5)
+print(SA)
+SA = SB           # (6)
+print(SA)
+CA = 'C'          # (7)
+print(CA)
+CA = "MAJUTERUS"  # (8)
+print(CA)
+BA = True         # (9)
+print(BA)
+BA = "#"          # (10)
+print(BA)
+
